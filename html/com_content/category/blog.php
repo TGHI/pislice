b66a86a95a20d8d6060212adbc66bc9096ed9122
Justin Renaud <tghidsgn@gmail.com>
2013-01-09 12:48:03 -0500

    initial commit

---
+++ html/com_content/category/blog.php
@@ -0,0 +1,87 @@
+<?php defined('_JEXEC') or die;
+/**
+ * piSlice - The simple, responsive template for Joomla! 3.0+
+ *
+ * @copyright   Copyright (C) 2012 Justin Renaud (tghidsgn@gmail.com)
+ * @license     GNU General Public License version 3 or later; see LICENCE.txt
+ */
+
+$app = JFactory::getApplication();
+$templateparams = $app->getTemplate(true)->params;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+$cparams = JComponentHelper::getParams('com_media');
+
+?>
+<section class="blog-roll<?php echo $this->pageclass_sfx;?>">
+  <?php if ($this->params->get('show_page_heading') != 0 or $this->params->get('show_category_title')): ?>
+  <h1> <?php echo $this->escape($this->params->get('page_heading')); ?>
+    <?php if ($this->params->get('show_category_title'))
+	{
+
+		echo '<span class="subheading-category">'.$this->category->title.'</span>';
+	}
+	?>
+  </h1>
+  <?php endif; ?>
+  <?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
+  <div class="category-desc">
+    <?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
+    <img src="<?php echo $this->category->getParams()->get('image'); ?>"/>
+    <?php endif; ?>
+    <?php if ($this->params->get('show_description') && $this->category->description) : ?>
+    <?php echo JHtml::_('content.prepare', $this->category->description, '', 'com_content.category'); ?>
+    <?php endif; ?>
+  </div>
+  <?php endif; ?>
+  <?php $leadingcount = 0; ?>
+  <?php if (!empty($this->lead_items)) : ?>
+  <div class="blog-item leading">
+<?php foreach ($this->lead_items as &$item) : ?>
+    <article class="<?php echo $item->state == 0 ? 'system-unpublished' : null; ?>">
+<?php
+				$this->item = &$item;
+				echo $this->loadTemplate('item');
+?>
+    </article>
+<?php
+			$leadingcount++;
+?>
+<?php endforeach; ?>
+  </div>
+  <?php endif; ?>
+  <?php
+	$introcount = (count($this->intro_items));
+	$counter = 0;
+?>
+  <?php if (!empty($this->intro_items)) : ?>
+  <?php foreach ($this->intro_items as $key => &$item) : ?>
+  <div class="blog-item">
+    <article class="item <?php echo $item->state == 0 ? ' system-unpublished' : null; ?>">
+<?php
+			$this->item = &$item;
+			echo $this->loadTemplate('item');
+?>
+    </article>
+    <div class="blog-item-separator"></div>
+  </div>
+  
+    <?php $counter++; ?>
+  <?php endforeach; ?>
+  <?php endif; ?>
+  <?php if (!empty($this->link_items)) : ?>
+  <?php echo $this->loadTemplate('links'); ?>
+  <?php endif; ?>
+  <?php if (is_array($this->children[$this->category->id]) && count($this->children[$this->category->id]) > 0 && $this->params->get('maxLevel') != 0) : ?>
+  <div class="cat-children">
+    <h3> <?php echo JTEXT::_('JGLOBAL_SUBCATEGORIES'); ?> </h3>
+    <?php echo $this->loadTemplate('children'); ?> </div>
+  <?php endif; ?>
+  <?php if (($this->params->def('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)) : ?>
+  <div class="pagination">
+    <?php  if ($this->params->def('show_pagination_results', 1)) : ?>
+    <p class="counter"> <?php echo $this->pagination->getPagesCounter(); ?> </p>
+    <?php endif; ?>
+    <?php echo $this->pagination->getPagesLinks(); ?> </div>
+  <?php  endif; ?>
+</section>
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php defined('_JEXEC') or die;
+/**
+ * piSlice - The simple, responsive template for Joomla! 3.0+
+ *
+ * @copyright   Copyright (C) 2012 Justin Renaud (tghidsgn@gmail.com)
+ * @license     GNU General Public License version 3 or later; see LICENCE.txt
+ */
+
+//Some handy variables
+$app			= JFactory::getApplication();
+$doc			= JFactory::getDocument();
+$this->language	= $doc->language;
+$this->direction= $doc->direction;
+$user			= JFactory::getUser();
+$option   		= $app->input->getCmd('option', '');
+$view		    = $app->input->getCmd('view', '');
+$layout   		= $app->input->getCmd('layout', '');
+$task     		= $app->input->getCmd('task', '');
+$itemid   		= $app->input->getCmd('Itemid', '');
+$sitename 		= $app->getCfg('sitename');
+
+// Get template params
+$tpl_param_generator		= $this->params->get('generator');
+$tpl_param_jquery			= $this->params->get('jquery');
+$tpl_param_framework		= $this->params->get('framework');
+
+// Add Stylesheets
+$doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
+$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap-responsive.min.css');
+$doc->addStyleSheet('http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic|Open+Sans:400,300,700,300italic,400italic');
+
+// Javascript Resources
+if ($tpl_param_framework == 1){
+	JHtml::_('behavior.framework', true);
+}else{
+	if (JRequest::getVar('layout') !== 'edit'){
+		$s = array('mootools','caption','core');
+		foreach($this->_scripts as $k => $v) {
+			foreach($s as $f) {
+				if(stristr($k, $f) !== false) {
+					unset($this->_scripts[$k]);
+				}
+			}
+		}
+	}
+	$this->_script = preg_replace('%window\.addEvent\(\'load\',\s*function\(\)\s*{\s*new\s*JCaption\(\'img.caption\'\);\s*}\);\s*%', '', $this->_script);
+	$this->_script = preg_replace('%window\.addEvent\(\'domready\',\s*function\(\)\s*{\s*\$\$\(\'.hasTip\'\).each\(function\(el\)\s*{\s*var\s*title\s*=\s*el.get\(\'title\'\);\s*if\s*\(title\)\s*{\s*var\s*parts\s*=\s*title.split\(\'::\',\s*2\);\s*el.store\(\'tip:title\',\s*parts\[0\]\);\s*el.store\(\'tip:text\',\s*parts\[1\]\);\s*}\s*}\);\s*var\s*JTooltips\s*=\s*new\s*Tips\(\$\$\(\'.hasTip\'\),\s*{\s*maxTitleChars:\s*50,\s*fixed:\s*false}\);\s*}\);%', '', $this->_script);
+}
+
+if ($tpl_param_jquery == "local"){
+	$doc->addScript('templates/'.$this->template.'/js/lib/jquery/jquery.min.js');
+}elseif ($tpl_param_jquery == "cdn"){
+	$doc->addScript('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+}
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+<?php if ($tpl_param_generator == 0) $doc->_generator = ""; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<jdoc:include type="head" />
+</head>
+<body>
+<div class="container center">
+<div class="navbar-fixed-top" style="background:#000">test</div>
+  <jdoc:include type="message" />
+  <jdoc:include type="modules" name="article-top" style="none" />
+  <jdoc:include type="modules" name="article-top2" style="none" />
+  <jdoc:include type="component" />
+  <jdoc:include type="modules" name="debug" style="none" />
+</div>
+</body>
+</html>
