b4f223d70e6adfbdb4f855de377ede999cc18943
Justin Renaud <tghidsgn@gmail.com>
2013-01-10 13:02:01 -0500

    Removed stray div

--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -15,14 +15,11 @@ $cparams = JComponentHelper::getParams('com_media');
 ?>
 <section class="blog-roll<?php echo $this->pageclass_sfx;?>">
   <?php if ($this->params->get('show_page_heading') != 0 or $this->params->get('show_category_title')): ?>
-  <h1> <?php echo $this->escape($this->params->get('page_heading')); ?>
-    <?php if ($this->params->get('show_category_title'))
+  <h1><?php echo $this->escape($this->params->get('page_heading')); ?><?php if ($this->params->get('show_category_title'))
 	{
-
 		echo '<span class="subheading-category">'.$this->category->title.'</span>';
 	}
-	?>
-  </h1>
+	?></h1>
   <?php endif; ?>
   <?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
   <div class="category-desc">
@@ -66,7 +63,7 @@ $cparams = JComponentHelper::getParams('com_media');
 ?>
       <div style="float:left" class="span2 center article-date">
         <span class="narrow"><?php echo JHTML::date($item->publish_up,'M',true) ?></span>
-        <span class="big" ><?php echo JHTML::date($item->publish_up,'d',true) ?></span>
+        <span class="huge" ><?php echo JHTML::date($item->publish_up,'d',true) ?></span>
       </div>
       <div style="float:left" class="span10"><?php echo $this->loadTemplate('item'); ?></div>
       <br style="clear:both" />
@@ -92,5 +89,4 @@ $cparams = JComponentHelper::getParams('com_media');
     <?php endif; ?>
     <?php echo $this->pagination->getPagesLinks(); ?> </div>
   <?php  endif; ?>
-  </div>
 </section>
