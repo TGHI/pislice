94ddf9623418fa3579834a6b854b30a360893789
Justin Renaud <tghidsgn@gmail.com>
2013-02-27 12:14:13 -0500

    params are now checked and filled with defaults
    if they happen to be empty

--- lib/params.php
+++ lib/params.php
@@ -6,59 +6,63 @@
  * @license     GNU General Public License version 3 or later; see LICENCE.txt
  */
 
-$templateStyles = "";
+$db					= JFactory::getDBO();
+$query				= $db->getQuery(true);
+$params 			= $this->params;
+$templateStyles		= "";
 
-// Get template params
+//check for template params
+if($params == "{}"){
 
-$tpl_param_titleFont = 			$this->params->get('titleFont');
-$tpl_param_bodyFont = 			$this->params->get('bodyFont');
-$tpl_param_navFont = 			$this->params->get('navFont');
-$tpl_param_linkColour = 		$this->params->get('linkColour');
-$tpl_param_linkHover = 			$this->params->get('linkHover');
-$tpl_param_complementColour = 	$this->params->get('complementColour');
-$tpl_param_detailIconsColour = 	$this->params->get('detailIconsColour');
-$tpl_param_animations = 		$this->params->get('animations');
-$tpl_param_generator = 			$this->params->get('generator');
+  jimport('joomla.filesystem.file');
 
-// Google Fonts
+  $config = file_get_contents('templates/' . $template . '/config/default.config.json');
+  
+  $db->setQuery("UPDATE #__template_styles SET params = '$config' WHERE template = '$template' LIMIT 1");
+  $result = $db->query();
 
-if($tpl_param_titleFont || $tpl_param_bodyFont || $tpl_param_navFont || $tpl_param_complementColour){
+  $template_params = json_decode($config);
 
-  $fonts = array($tpl_param_titleFont,$tpl_param_bodyFont,$tpl_param_navFont);
-  $googleFonts = implode("|",$fonts);
+}else{
 
-  for($i = 0; $i < count($fonts);$i++){
-    if(strpos($fonts[$i],':')){
-      $fonts[$i] = substr($fonts[$i], 0, strpos($fonts[$i],':'));
-    }
-	$fonts[$i] = str_replace('+', ' ',$fonts[$i]);
-  }
-
-  $templateStyles .= "    h1,h2,h3,.article-introtext,blockquote{font-family:\"" . $fonts[0] . "\",serif}\n";
-  $templateStyles .= "    select,input,body{font-family:\"" . $fonts[1] . "\",sans-serif}\n";
-  $templateStyles .= "    .narrow{font-family:\"" . $fonts[2] . "\",sans-serif}\n";
+  $template_params = json_decode($params);
 
 }
 
+// Google Fonts
+
+$fonts = array($template_params->titleFont,$template_params->bodyFont,$template_params->navFont);
+$googleFonts = implode("|",$fonts);
+
+for($i = 0; $i < count($fonts);$i++){
+  if(strpos($fonts[$i],':')){
+    $fonts[$i] = substr($fonts[$i], 0, strpos($fonts[$i],':'));
+  }
+  $fonts[$i] = str_replace('+', ' ',$fonts[$i]);
+}
+
+$templateStyles .= "    h1,h2,h3,.article-introtext,blockquote{font-family:\"" . $fonts[0] . "\",serif}\n";
+$templateStyles .= "    select,input,body{font-family:\"" . $fonts[1] . "\",sans-serif}\n";
+$templateStyles .= "    .narrow{font-family:\"" . $fonts[2] . "\",sans-serif}\n";
+
 // Theme Colours
 
-$templateStyles .= "    a{color:" . $tpl_param_linkColour . "}\n";
-$templateStyles .= "    a:hover{" . $tpl_param_linkHover . "}\n";
-$templateStyles .= "    .article-details dd span,.article-index ul a:before{background:" . $tpl_param_detailIconsColour . "}\n";
-$templateStyles .= "    .blog-item-separator{border-bottom:5px dotted " . $tpl_param_complementColour . "}\n";
-$templateStyles .= "    .article-date{color:" . $tpl_param_complementColour . "}\n";
-$templateStyles .= "    .article-index ul a:hover:before,.article-index ul a.active:before{background:" . $tpl_param_complementColour . "}";
-
+$templateStyles .= "    a{color:" . $template_params->linkColour . "}\n";
+$templateStyles .= "    a:hover{" . $template_params->linkHover . "}\n";
+$templateStyles .= "    .article-details dd span,.article-index ul a:before{background:" . $template_params->detailIconsColour . "}\n";
+$templateStyles .= "    .blog-item-separator{border-bottom:5px dotted " . $template_params->complementColour . "}\n";
+$templateStyles .= "    .article-date{color:" . $template_params->complementColour . "}\n";
+$templateStyles .= "    .article-index ul a:hover:before,.article-index ul a.active:before{background:" . $template_params->complementColour . "}";
 
 // Theme Misc
 
-if ($tpl_param_animations == 0){
+if ($template_params->animations == 0){
   $templateStyles .= "\n    .anim,.span2{transition:none;-webkit-transition:none;-moz-transition:none;-o-transition:all 0;transform:none !important}";
 }
 
 // Misc Params
 
-if ($tpl_param_generator == 0) {
+if ($template_params->generator == 0) {
   $doc->_generator = "";
 }
 
