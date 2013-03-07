5e55c5811705f7e16b33ec603855004e4afdf53f
Justin Renaud <tghidsgn@gmail.com>
2013-03-07 16:42:41 -0500

    broke down footer into 3 modules

--- index.php
+++ index.php
@@ -67,29 +67,19 @@ $doc->addStyleDeclaration($templateStyles);
     </div>
   </div>
 </div>
-<div class="footer container-fluid" style="">
+<div class="footer container-fluid">
   <footer>
     <div class="row-fluid clearfix">
       <div class="span2"></div>
       <div class="footer-inner span7 small">
         <div class="span4">
-          <h2 style="margin:0 0 10px 0;padding:0 0 5px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">piSlice Footer</h2>
-          Copyright &copy; 2013 <a href="#">TGHI</a>. All Rights Reserved.<br/>
-          piSlice is released under the <a href="http://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License Version 3</a> or later. <br />
-          Fork this project over at <a href="https://github.com/TGHI/pislice">Github</a>.
+          <jdoc:include type="modules" name="footer-left" style="none" />
         </div>
         <div class="span4">
-          <h2 style="margin:0 0 10px 0;padding:0 0 5px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">Joomla Footer Module</h2>
-          <jdoc:include type="modules" name="footer" style="none" />
+          <jdoc:include type="modules" name="footer-middle" style="none" />
         </div>
         <div class="span4">
-        <h2 style="margin:0;padding:0 0 5px 0;margin:0 0 10px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">Search</h2>
-          <jdoc:include type="modules" name="footer-search" style="none" />
-          <h2 style="margin:0;padding:0 0 5px 0;margin:10px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">Social</h2>
-          <span class="social icon-github"></span>
-          <span class="social icon-facebook"></span>
-          <span class="social icon-twitter"></span>
-          <span class="social icon-googleplus"></span>
+          <jdoc:include type="modules" name="footer-right" style="none" />
         </div>
       </div>
     </div>
