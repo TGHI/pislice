f5a7dd3e679663ac237a5c425b0a951a9e0f8d71
Justin Renaud <tghidsgn@gmail.com>
2013-03-07 13:10:17 -0500

    Fluid navbar fixed, begining of footer.

--- index.php
+++ index.php
@@ -39,9 +39,15 @@ $doc->addStyleDeclaration($templateStyles);
 <body class="anim">
 <div class="navbar navbar-fixed-top container-fluid">
   <div class="navbar-inner row-fluid">
-    <a href="<?php echo $this->baseurl; ?>/" class="brand span2"><img src="templates/<?php echo $template; ?>/img/pislice-logo.png" alt="" /></a>
-    <jdoc:include type="modules" name="navigation" style="none" />
-    <jdoc:include type="modules" name="search" style="none" />
+    <div class="span2">
+    </div>
+    <div class="span7">
+      <a href="<?php echo $this->baseurl; ?>/" class="brand span2"><img src="templates/<?php echo $template; ?>/img/pislice-logo.png" alt="" /></a>
+      <jdoc:include type="modules" name="navigation" style="none" />
+    </div>
+    <div class="span3">
+      <jdoc:include type="modules" name="search" style="none" />
+    </div>
   </div>
 </div>
 <div class="container-fluid">
@@ -60,6 +66,34 @@ $doc->addStyleDeclaration($templateStyles);
       <jdoc:include type="modules" name="sidebar-right" style="none" />
     </div>
   </div>
+</div>
+<div class="footer container-fluid" style="">
+  <footer>
+    <div class="row-fluid clearfix">
+      <div class="span2"></div>
+      <div class="footer-inner span7 small">
+        <div class="span4">
+          <h2 style="margin:0 0 10px 0;padding:0 0 5px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">piSlice Footer</h2>
+          Copyright &copy; 2013 <a href="#">TGHI</a>. All Rights Reserved.<br/>
+          piSlice is released under the <a href="http://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License Version 3</a> or later. <br />
+          Fork this project over at <a href="https://github.com/TGHI/pislice">Github</a>.
+        </div>
+        <div class="span4">
+          <h2 style="margin:0 0 10px 0;padding:0 0 5px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">Joomla Footer Module</h2>
+          <jdoc:include type="modules" name="footer" style="none" />
+        </div>
+        <div class="span4">
+        <h2 style="margin:0;padding:0 0 5px 0;margin:0 0 10px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">Search</h2>
+          <jdoc:include type="modules" name="footer-search" style="none" />
+          <h2 style="margin:0;padding:0 0 5px 0;margin:10px 0;border-bottom:1px solid #dedfe3;box-shadow:0 1px 0 #fff">Social</h2>
+          <span class="social icon-github"></span>
+          <span class="social icon-facebook"></span>
+          <span class="social icon-twitter"></span>
+          <span class="social icon-googleplus"></span>
+        </div>
+      </div>
+    </div>
+  </footer>
 </div>
 </body>
 </html>
