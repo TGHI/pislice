8aebcf0da935087e0b51ae2d2b8f7e9e891fdea2
Justin Renaud <tghidsgn@gmail.com>
2013-03-12 14:13:55 -0400

    initial commit

---
+++ html/com_search/default_error.php
@@ -0,0 +1,15 @@
+<?php defined('_JEXEC') or die;
+/**
+ * piSlice - The simple, responsive template for Joomla! 3.0+
+ *
+ * @copyright   Copyright (C) 2013 Justin Renaud (tghidsgn@gmail.com)
+ * @license     GNU General Public License version 3 or later; see LICENCE.txt
+ */
+
+?>
+
+<?php if ($this->error) : ?>
+<div class="error">
+			<?php echo $this->escape($this->error); ?>
+</div>
+<?php endif; ?>
