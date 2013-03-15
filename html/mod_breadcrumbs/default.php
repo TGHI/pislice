af370e85e18b2d1e6e69a95eefdf0c3af1327e1a
Justin Renaud <tghidsgn@gmail.com>
2013-03-15 15:20:19 -0400

    code uqality

--- html/mod_breadcrumbs/default.php
+++ html/mod_breadcrumbs/default.php
@@ -6,9 +6,9 @@
  * @license     GNU General Public License version 3 or later; see LICENCE.txt
  */
 
-$app = JFactory::getApplication();
-$option	= $app->input->getCmd('option', '');
-$searchword	= $app->input->getCmd('searchword', '');
+ $app = JFactory::getApplication();
+ $option	= $app->input->getCmd('option', '');
+ $searchword	= $app->input->getCmd('searchword', '');
 
 ?>
 <div class="breadcrumbs<?php echo $moduleclass_sfx; ?>">
