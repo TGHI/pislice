e905ddf664dd3ed5c439cb813dbef88aefa8040f
Justin Renaud <tghidsgn@gmail.com>
2013-03-27 11:30:12 -0400

    fixed opengraph handling
    article catid's are now determined from the article object as opposed to
    the url string (since some components like tags don't pass it).

--- lib/opengraph.php
+++ lib/opengraph.php
@@ -9,41 +9,45 @@
 $metaArray = array();
 
 if ($option == "com_content"){
-
+	
+  function getCategoryName($id){
+    
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query->select ('alias');
+    $query->from('#__categories');
+    $query->where("id='$id'");
+	
+    $db->setQuery($query);
+	$db->loadObjectList();
+    return $db->loadObjectList()[0]->alias;
+  
+  }
+  
   $metaArray["og:title"]       = $doc->title;
   $metaArray["og:description"] = $doc->description;
   $metaArray["og:type"]        = $view;
   $metaArray["og:url"]         = key($doc->_links);
-
+  
   if ($view == "category"){
     $category = JRequest::getVar('id');
-  }else{
-	$category = JRequest::getVar('catid'); 
+    $metaArray["og:section"] = getCategoryName($category);
   }
-
-  $db = JFactory::getDbo();
-  $query = $db->getQuery(true);
-  $query->select ('alias');
-  $query->from('#__categories');
-  $query->where("id='$category'");
   
-  $db->setQuery($query);
-  $results = $db->loadObjectList();
-
-  $metaArray["og:section"] = $results[0]->alias;
-
   if ($view == "article"){
-
+  
     $ids = explode(':',JRequest::getString('id'));
     $article_id = $ids[0];
-    $article = JTable::getInstance("content");
+	$article = JTable::getInstance("content");
     $article->load($article_id);
 
     $images = json_decode($article->images);
     $author = JFactory::getUser($article->created_by);
-
+  
+    $metaArray["og:section"] = getCategoryName($article->catid);
     $metaArray["og:image"]  = $base_url . $images->image_intro;
     $metaArray["og:author"] = $author->name;
     $metaArray["article:published_time"] = $article->publish_up;
   }
+  
 }
