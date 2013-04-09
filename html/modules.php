4a2cda2b0b6a0fc1360fed9d61af4ec74429f472
Justin Renaud <tghidsgn@gmail.com>
2013-04-09 15:46:47 -0400

    code quality

--- html/modules.php
+++ html/modules.php
@@ -13,10 +13,10 @@ function modChrome_pi_default($module, &$params, &$attribs){
   $moduleArray = array();
 
   if (isset( $attribs['shadows'] )){
-	  array_push($moduleArray, "folded-shadow");
+    array_push($moduleArray, "folded-shadow");
   }
   if ($params->get('moduleclass_sfx')){
-	 array_push($moduleArray, htmlspecialchars($params->get('moduleclass_sfx')));
+    array_push($moduleArray, htmlspecialchars($params->get('moduleclass_sfx')));
   }
   
   $moduleClasses = implode(" ", $moduleArray);
@@ -25,7 +25,7 @@ function modChrome_pi_default($module, &$params, &$attribs){
 <?php if (!empty ($module->content)):?>
 <<?php echo htmlspecialchars($params->get('module_tag')); ?> class="moduletable span<?php echo htmlspecialchars($params->get('bootstrap_size')); ?> <?php echo $moduleClasses; ?>">
 <?php if($module->showtitle): ?>
-  <<?php echo htmlspecialchars($params->get('header_tag')); ?> class="moduletitle <?php echo htmlspecialchars($params->get('header_class')); ?>"><?php echo $module->title; ?></<?php echo htmlspecialchars($params->get('header_tag')); ?>>
+<<?php echo htmlspecialchars($params->get('header_tag')); ?> class="moduletitle <?php echo htmlspecialchars($params->get('header_class')); ?>"><?php echo $module->title; ?></<?php echo htmlspecialchars($params->get('header_tag')); ?>>
   <?php echo $module->content; ?>
 <?php endif; ?>
 </<?php echo htmlspecialchars($params->get('module_tag')); ?>>
