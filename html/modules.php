c952ce50eab69a8308313713a65a18cbb18a8b0a
Justin Renaud <tghidsgn@gmail.com>
2013-08-26 15:27:38 -0400

    remove round icons

--- html/modules.php
+++ html/modules.php
@@ -33,7 +33,7 @@ function modChrome_pi_default($module, &$params, &$attribs)
 <?php if (!empty($module->content)):?>
 <<?php echo htmlspecialchars($params->get('module_tag')); ?> class="pi_default moduletable <?php echo $bootstrapSize; ?> <?php echo $moduleClasses; ?>">
 <?php if($module->showtitle): ?>
-<<?php echo htmlspecialchars($params->get('header_tag')); ?> class="moduletitle"><i class="round inset-3d <?php echo htmlspecialchars($params->get('header_class')); ?>"></i><?php echo $module->title; ?></<?php echo htmlspecialchars($params->get('header_tag')); ?>>
+<<?php echo htmlspecialchars($params->get('header_tag')); ?> class="moduletitle"><i style="font-size:19px" class="<?php echo htmlspecialchars($params->get('header_class')); ?>"></i><?php echo $module->title; ?></<?php echo htmlspecialchars($params->get('header_tag')); ?>>
 <?php endif; ?>
 <div class="modulecontent">
   <?php echo $module->content; ?>
